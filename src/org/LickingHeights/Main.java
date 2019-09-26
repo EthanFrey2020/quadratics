ac888a37ce4a3e2ec7ca929ca04e51ff7dc86127
Ethan <ethan.frey.2020@stuent.lhschools.org>
2019-09-26 10:24:04 -0400

    IMPLEMENTED IF ELSE LADDER

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -7,21 +7,27 @@ public class Main {
         //TODO DECLARE VARIABLES
             double a,b,c,discriminant,axisOfSymetery,x,y;
             String parabolaFacing;
-
         //TODO INITIALIZE VARIABLES MANUALLY
             a=5;
             b=3;
             c=1;
-
         //TODO CALCULATIONS
             axisOfSymetery = -b/(2*a);
             discriminant = (b*b)-4*(a*c);
-
+                if(discriminant >0){
+                    System.out.println("There are two real solutions");
+                }
+                else if (discriminant == 0) {
+                    System.out.println("There is one solution");
+                }
+                else {
+                    System.out.println("There are NO real solutions");
+                }
         //TODO PRINT RESULTS
             System.out.println("Axis of Symmetry: "+axisOfSymetery);
             System.out.println("Discriminant: "+discriminant);
-
         //TODO CHECK ACCURACY
+
 
         //TODO INITIALIZE FOR SCANNER
 
